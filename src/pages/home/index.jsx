0f24bb7a72dde5bfcf59cd993b9865a7ac07817b
michael-leonov <michael.leonov96@mail.ru>
2023-03-01 17:30:32 +0300

    wip: add home page and 404

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderButton from '../../components/header/header-button';
+import MainButton from '../../components/main-button';
+
+function Home() {
+  return (
+    <main>
+      home page
+      <MainButton>Сохранить</MainButton>
+    </main>
+  );
+}
+
+export default Home;
