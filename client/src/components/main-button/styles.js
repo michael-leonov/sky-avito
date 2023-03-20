b8e8c26793e2ddcc1abe9d53b640f08c0af11f1b
Michael <michael.leonov96@mail.ru>
2023-03-20 23:32:13 +0300

    moved main button component according to structure project

---
+++ client/src/components/main-button/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #009ee4;
+  padding: 13px 37px;
+  border-radius: 6px;
+  cursor: pointer;
+  border: none;
+  color: white;
+  font-family: 'Roboto';
+  font-weight: 400;
+  transition: background-color 0.2s ease-in;
+
+  :hover {
+    background-color: #0080c1;
+  }
+`;
+
+export const InactiveButton = styled(Button)`
+  background-color: #d9d9d9;
+  cursor: auto;
+
+  :hover {
+    background-color: #d9d9d9;
+  }
+`;
