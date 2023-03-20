9253cb3d8a3a9dd1dc5739399fa72a7c4594a6b8
Michael <michael.leonov96@mail.ru>
2023-03-20 23:34:41 +0300

    add return back component

---
+++ client/src/components/return-home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BackHomeBlock = styled.div`
+  margin-bottom: 43px;
+`;
+
+export const ReturnHomeWrapper = styled.div`
+  display: flex;
+  column-gap: 60px;
+`;
