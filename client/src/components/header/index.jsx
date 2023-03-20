02a64ac26208fe519c9dd8588c10c1aacc39b2f5
Michael <michael.leonov96@mail.ru>
2023-03-20 23:29:02 +0300

    add header component

---
+++ client/src/components/header/index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import * as S from './styles';
+import HeaderButton from './header-button';
+import { StyledContainer, Overlay } from '../../global-styles';
+import AdvForm from '../adv-form';
+import { userLogout } from '../../redux/slices/userActions';
+
+function Header() {
+  const [visibleAddAdv, setVisibleAddAdv] = useState();
+
+  const { userInfo, userToken } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(userLogout());
+  };
+
+  return (
+    <S.Header>
+      <StyledContainer>
+        <S.HeaderInner>
+          {!userToken ? (
+            <Link to="/login">
+              <HeaderButton>Вход в личный кабинет</HeaderButton>
+            </Link>
+          ) : (
+            <S.HeaderAuthButtons>
+              <HeaderButton onClick={() => setVisibleAddAdv(true)}>
+                Разместить объявление
+              </HeaderButton>
+              <Link to={`/profile/${userInfo?.id}`}>
+                <HeaderButton>Личный кабинет</HeaderButton>
+              </Link>
+              <HeaderButton onClick={logout}>Выйти</HeaderButton>
+            </S.HeaderAuthButtons>
+          )}
+        </S.HeaderInner>
+      </StyledContainer>
+
+      {visibleAddAdv && (
+        <Overlay>
+          <AdvForm
+            closeForm={() => setVisibleAddAdv(false)}
+            isEditStatusForm={false}
+          />
+        </Overlay>
+      )}
+    </S.Header>
+  );
+}
+
+export default Header;
