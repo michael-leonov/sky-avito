b2bc11ed8306e10fec60ed79d0e762b3ffe1adee
Michael <michael.leonov96@mail.ru>
2023-03-20 23:27:23 +0300

    add adv component

---
+++ client/src/components/adv/styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Adv = styled.div``;
+
+export const AdvImage = styled.img`
+  width: 270px;
+  height: 270px;
+  margin-bottom: 20px;
+`;
+
+export const AdvTitle = styled.p`
+  font-size: 22px;
+  margin-bottom: 10px;
+  color: #009ee4;
+  font-weight: 500;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  display: -webkit-box;
+`;
+
+export const AdvPrice = styled.p`
+  font-size: 22px;
+  margin-bottom: 10px;
+  color: #000;
+  font-weight: 500;
+`;
+
+export const AdvLocation = styled.p`
+  font-size: 16px;
+  margin-bottom: 4px;
+  color: #5f5f5f;
+`;
+
+export const AdvDataRelease = styled.p`
+  font-size: 16px;
+  margin-bottom: 4px;
+  color: #5f5f5f;
+`;
