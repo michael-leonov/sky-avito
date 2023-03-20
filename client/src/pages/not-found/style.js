a89d02af97907d0f26d8fbd880ed9ca1385310c3
Michael <michael.leonov96@mail.ru>
2023-03-20 23:46:27 +0300

    add 404 page

---
+++ client/src/pages/not-found/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NotFoundInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NotFoundText = styled.p`
+  font-size: 64px;
+  color: #009ee4;
+  margin-bottom: 10px;
+`;
